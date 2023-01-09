4743ce6631d6fad67d33e27b965c29ccc235d47d
Ksynlsn <ksynlsn@gmail.com>
2023-01-09 14:47:13 -0800

    adds data types and variables

--- chapter1.py
+++ chapter1.py
@@ -1,1 +1,28 @@
 # Chapter One
+
+cats = ["Moose", "Ashes", "Willy", "Pat", "Suily"]
+for c in cats:
+    invitation = "Hello " + c + "! Please come to my stupid cat party"
+    print(invitation)
+
+# to view this output in the terminal, type 'python3 filename.py'   Yay!
+
+# Syntax rules pertain to TOKENS and STRUCTURES. TOKENS are basic elements of the language
+# TOKENS are the basic elements of the lanuage
+#                   like *WORDS *NUMBERS *PARENTHESES *COMMAS
+
+print("Happy New Year For", 2023) # this has 6 tokens, a function name, (, a string, a comma, a number, )
+
+# DATA TYPES OH BOY
+animals = ["cat", "dog", "chicken", "fly"]
+fruit = ("apple", "banana", "cherry")
+person1 = {"Name" : "Kacie", "Age" : 33}
+print(type("string"))
+print(type(666))
+print(type(3.14))
+print(type(animals))
+print(type(fruit))
+print(type(person1))
+print(person1)
+
+# VARIABLES

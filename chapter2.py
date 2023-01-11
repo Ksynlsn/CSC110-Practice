ec55ef65b11727b772e3f2504f4007742006c8af
Ksynlsn <ksynlsn@gmail.com>
2023-01-11 13:00:35 -0800

    evaluating expressions

---
+++ chapter2.py
@@ -0,0 +1,10 @@
+# Chapter 2
+
+# Evaluating Expressions
+# EXPRESSION = a combination of values, operators, and calls to functions
+
+1 + 1
+
+print(len("how many characters are in this string?")) #including spaces
+print(len("hello"))
+print(len("hello 123"))

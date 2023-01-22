6b747f0c17d884459e6bd5c0dcbd03db798a4663
Ksynlsn <ksynlsn@gmail.com>
2023-01-21 22:43:45 -0800

    adds turtle exercises

---
+++ ch4-exercises.py
@@ -0,0 +1,52 @@
+numSides = int(input("How many sides should there be?:"))
+sideLength = int(input("How long should each side be?:"))
+fillcolor = input("What color should this polygon be?")
+
+import turtle
+userTurtle = turtle.Turtle()
+userTurtle.fillcolor(fillcolor)
+
+userTurtle.begin_fill()
+for num in range(numSides):
+    userTurtle.forward(sideLength)
+    userTurtle.left(360/numSides)
+    
+userTurtle.end_fill()
+
+import turtle
+blue = turtle.Turtle()
+blue.shape("arrow")
+blue.color("green")
+
+# An equilateral triangle
+for side in ['one', 'two', 'three']:
+    blue.forward(150)
+    blue.left(120)
+
+# A square
+blue.penup()
+blue.backward(125)
+blue.down()
+for num in range(4):
+    blue.forward(200)
+    blue.left(90)
+
+blue.penup()
+blue.right(90)
+blue.forward(125)
+blue.down()
+# An octogon
+blue.color('purple')
+for i in range(8):
+    blue.backward(100)
+    blue.right(45)
+    
+# A hexagon
+blue.color('pink')
+blue.left(90)
+blue.penup()
+blue.forward(65)
+blue.down()
+for i in range(6):
+    blue.forward(100)
+    blue.left(360/6)

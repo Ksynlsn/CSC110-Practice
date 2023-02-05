a6d9f6cd8f6e867fe15aaaef2fa2dfdcd2364481
Ksynlsn <ksynlsn@gmail.com>
2023-02-04 16:49:44 -0800

    adds week 5 homework: turtle graphics

--- turtles/ex1.py
+++ turtles/ex1.py
@@ -1,54 +1,40 @@
+# CSC 110 Week 5 Assignment
 import turtle
 wn = turtle.Screen()
-wn.bgcolor('black')
 tri = turtle.Turtle()
-tri.color('yellow')
 poly = turtle.Turtle()
-poly.color('pink')
 hexa = turtle.Turtle()
-hexa.color('red')
 s = turtle.Turtle()
-s.color('green')
 
-def drawPolygon(t, s):
+def drawPolygon(t, s, l):
     for i in range(s):
-        t.forward(100)
+        t.forward(l)
         t.left(360 / s)
+
 # Tri draws a triangle
 tri.up()
-tri.goto(-200, 200)
+tri.goto(-300, 75)
 tri.down()
-tri.fillcolor('yellow')
-tri.begin_fill()
-drawPolygon(tri, 3)
-tri.end_fill()
+drawPolygon(tri, 3, 200)
 
-# Poly draws a polygon
+# Poly draws a pentagon
 poly.up()
-poly.goto(-200, -200)
+poly.goto(-275, -250)
 poly.down()
-poly.fillcolor('pink')
-poly.begin_fill()
-drawPolygon(poly, 5)
-poly.end_fill()
+drawPolygon(poly, 5, 150)
 
 # S draws a square
 s.up()
-s.goto(200, 200)
+s.goto(50, 75)
 s.down()
-s.fillcolor('green')
-s.begin_fill()
-drawPolygon(s, 4)
-s.end_fill()
+drawPolygon(s, 4, 150)
 
 # Hexa draws a hexagon
 hexa.up()
-hexa.goto(200, -200)
+hexa.goto(75, -250)
 hexa.down()
-hexa.fillcolor('red')
-hexa.begin_fill()
-drawPolygon(hexa, 6)
-hexa.end_fill()
+drawPolygon(hexa, 6, 125)
+
 
 
 
